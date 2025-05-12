5323dc2711b4321797fc8debbc51247500339520
AlexImbacuan <alex.imbacuan@correounivalle.edu.co>
2025-05-11 22:49:08 -0500

    modificando ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,29 @@
-estudiante = input ("Escriba el nombre de un estudiante: ")
-a = float(input("Ingrese una calificaion: "))
-b = float(input("Ingrese una segunda calificacion: "))
-c = float (input("Ingrese una tercera calificacion: "))
-Suma = float (a + b + c)
-Promedio = float ( Suma / 3)
-print ("El promedio de notas del estudiante ",estudiante, "es de: ", Promedio)
+#clase estudiante con metodo con metodo para calcular promedio
+
+class estudiante:
+    materias={}
+    def __init__ (self):
+        self.materias
+
+    def agregarmateria(self):
+        self.materias[input("Escriba la materia que quiere agregar: ")]=0 
+    def setnotas(self):
+        for indice, materia in enumerate(self.materias):
+            self.materias[materia]=float(input(f"dijite la nota de {materia}: "))
+    
+    def calcularpromedio(self):
+        promedio = 0
+        suma = 0
+        for indice, materia in enumerate(self.materias):
+            suma += self.materias[materia]
+        return f"El promedio es: {suma/len(self.materias)} "
+        
+
+
+estudiante1= estudiante()
+estudiante1.agregarmateria()
+estudiante1.agregarmateria()
+estudiante1.setnotas()
+
+
+print(estudiante1.calcularpromedio())
